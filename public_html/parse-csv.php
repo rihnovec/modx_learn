19a917fdc980fa6ffb81beaf50d65a9242db1572
Anton Rikhnovets <rihnovec93@mail.ru>
2022-03-31 10:57:43 +0900

    Дорабатывает скрипты для добавления людей из CSV

--- public_html/parse-csv.php
+++ public_html/parse-csv.php
@@ -1,12 +1,12 @@
 <?php
 
-echo "<pre>";
+$arrPeople = [];
+
 if (($handle = fopen($_SERVER['DOCUMENT_ROOT'] . '/import-csv/people.csv', 'r')) !== FALSE) { // Check the resource is valid
   while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) { // Check opening the file is OK!
-      print_r($data); // Array
+      $arrPeople[] = $data;
   }
   fclose($handle);
 }
-echo "</pre>";
 
-
+return $arrPeople;
